472dbdee92a3d3440565d6465f8b3881ce305d86
Anthony <gin.anthonic@gmail.com>
2026-03-17 10:56:23 +0100

    MVP; added Defolderize and ReqursiveDefolderize functions, small refactors

--- Defolderizer/Program.cs
+++ Defolderizer/Program.cs
@@ -1,32 +1,58 @@
 ﻿using System;
 using System.IO;
+using System.Transactions;
 using Microsoft.VisualBasic;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
         string currentWorkingDirectory = "C:\\Users\\Work\\Desktop\\testing\\gin";
 
-        Unfold(currentWorkingDirectory);
+        RecursiveDefolderize(currentWorkingDirectory);
     }
+
+
+    public static void Defolderize(string currentDirectory)
+    {
+        foreach (string directory in Directory.GetDirectories(currentDirectory))
+        {
+           Unfold(directory); 
+        }
+    }
+
+
+    public static void RecursiveDefolderize(string currentDirectory)
+    {
+        while (Directory.GetDirectories(currentDirectory).Length > 0)
+        {
+            Defolderize(currentDirectory);
+        }
+    }
+
 
     public static void Unfold(string currentDirectory)
     {
         string parentDirectory = Directory.GetParent(currentDirectory).FullName;
-       
+        MoveFiles(currentDirectory,parentDirectory);
+        MoveDirectories(currentDirectory,parentDirectory);
+        Directory.Delete(currentDirectory);
+    }
+
+
+    public static void MoveFiles(string currentDirectory, string parentDirectory)
+    {
         string[] files = Directory.GetFiles(currentDirectory);
         foreach (string file in files){
-            string fileName = file.Substring(file.LastIndexOf("\\")+1);
-            fileName = fileName.Substring(0,fileName.LastIndexOf("."));
-            string fileExtension = file.Substring(file.LastIndexOf("."));
-            Console.WriteLine(fileName);
-            string filePath = currentDirectory+"\\"+fileName+fileExtension;
+            string filePath = file;
+            string fileName = GetExtensionlessFileName(filePath);
+            string fileExtension = filePath.Substring(filePath.LastIndexOf("."));
             string newFilePath = parentDirectory+"\\"+fileName+fileExtension;
 
             while (File.Exists(newFilePath))
-            {
+            {   
                 Console.WriteLine(fileName+" already exists in "+parentDirectory+"... Renaming...");
-                FileSystem.Rename(filePath,currentDirectory+"\\"+fileName+"_copy"+fileExtension);
+                File.Move(filePath, currentDirectory+"\\"+fileName+"_copy"+fileExtension);
                 fileName = fileName+"_copy";
                 filePath = currentDirectory+"\\"+fileName+fileExtension;
                 newFilePath = parentDirectory+"\\"+fileName+fileExtension;
@@ -35,14 +61,35 @@ internal class Program
             Console.WriteLine("Moving "+fileName);
             File.Move(filePath,newFilePath);
         }
+    }
 
+    public static void MoveDirectories(string currentDirectory, string parentDirectory)
+    {
         foreach(string directory in Directory.GetDirectories(currentDirectory))
         {
+            string directoryPath = directory;
             string directoryName = directory.Substring(directory.LastIndexOf("\\")+1);
-            Console.WriteLine("Moving "+directoryName+" to "+parentDirectory+"\\"+directoryName);
-            Directory.Move(directory,parentDirectory+"\\"+directoryName);
-        }
+            string newDirectoryPath = parentDirectory + "\\" + directoryName;
 
-        Directory.Delete(currentDirectory);
+            while (Directory.Exists(newDirectoryPath))
+            {
+                Console.WriteLine(directoryName + " already exists in " + parentDirectory + "... Renaming...");
+                Directory.Move(directoryPath, currentDirectory + "\\" + directoryName + "_copy");
+                directoryName = directoryName + "_copy";
+                directoryPath = currentDirectory + "\\" + directoryName;
+                newDirectoryPath = parentDirectory + "\\" + directoryName;
+            }
+
+            Console.WriteLine("Moving "+directoryName+" to "+parentDirectory+"\\"+directoryName);
+            Directory.Move(directoryPath,newDirectoryPath);
+        }
     }
+
+    public static string GetExtensionlessFileName(string filePath)
+    {
+        string fileName = filePath.Substring(filePath.LastIndexOf("\\")+1);
+        fileName = fileName.Substring(0,fileName.LastIndexOf("."));
+        return(fileName);
+    }
+  
 }
