433f8ee5ac2ff6e49e4bc85f04b1d8935c74d248
Gin-Anthonic <gin.anthonic@gmail.com>
2026-05-11 12:01:14 +0200

    added unit tests for ArgValidation

--- Defolderizer.Tests/ArgValidationTests.cs
+++ Defolderizer.Tests/ArgValidationTests.cs
@@ -5,8 +5,46 @@ namespace Defolderizer.Tests;
 
 public class ArgValidationTests {
 
-    [Fact]
-    public void ArgValidation() {
-        Assert.True(true);
+    [Theory]
+    [InlineData("unfold")]
+    [InlineData("defolderize")]
+    [InlineData("recursive")]
+    public void ArgValidationShouldSucceed(string mode) {
+        ArgValidationService argValidationService = new ArgValidationService();
+        ArgValidationService.ValidationResult expect = ArgValidationService.ValidationResult.Ok;
+
+        ArgValidationService.ValidationResult actual = argValidationService.ValidateArgs(["C:\\",mode]);
+       
+        Assert.Equal(expect,actual);
     }
+
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("something else")]
+    public void ArgValidationShouldFail(string mode) {
+        ArgValidationService argValidationService = new ArgValidationService();
+        ArgValidationService.ValidationResult expect = ArgValidationService.ValidationResult.InvalidMode;
+
+        ArgValidationService.ValidationResult actual = argValidationService.ValidateArgs(["C:\\", mode]);
+
+        Assert.Equal(expect, actual);
+    }
+
+
+    [Theory]
+    [InlineData(new object[] { new string[] { "","","" } })]
+    [InlineData(new object[] { new string[] { "" } })]
+    [InlineData(new object[] { new string[] {  } })]
+    public void ArgValidationWrongCountShouldFail(string[] args) {
+        ArgValidationService argValidationService = new ArgValidationService();
+        ArgValidationService.ValidationResult expect = ArgValidationService.ValidationResult.InvalidCount;
+
+        ArgValidationService.ValidationResult actual = argValidationService.ValidateArgs(args);
+
+        Assert.Equal(expect, actual);
+    }
+
 }
+
+
