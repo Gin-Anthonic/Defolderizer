f9ad5abdce052e3dd1af308a3cfc91e794a1498b
Gin-Anthonic <gin.anthonic@gmail.com>
2026-05-11 11:30:36 +0200

    added testing project(no tests yet)

---
+++ Defolderizer.Tests/ArgValidationTests.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+using Defolderizer.Services;
+
+namespace Defolderizer.Tests;
+
+public class ArgValidationTests {
+
+    [Fact]
+    public void ArgValidation() {
+        Assert.True(true);
+    }
+}
