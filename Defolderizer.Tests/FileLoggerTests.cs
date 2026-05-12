1577062e4bc084c79ba8ef3ec58a63c4d021510a
Gin-Anthonic <gin.anthonic@gmail.com>
2026-05-12 10:21:48 +0200

    finalized fileLogger tests

--- Defolderizer.Tests/FileLoggerTests.cs
+++ Defolderizer.Tests/FileLoggerTests.cs
@@ -6,19 +6,33 @@ namespace Defolderizer.Tests;
 public class FileLoggerTests {
 
     [Fact]
-    public void WriteLogEntryShouldCreateLog() {
-
+    public void WriteLogEntryShouldCreateFiles() {
         FileLoggingService loggingService = new FileLoggingService("");
-
         loggingService.WriteLogEntry("this is a user log","this is a developer log");
 
         FileInfo UserFile = new FileInfo("userLog.txt");
         FileInfo DeveloperFile = new FileInfo("developerLog.txt");
+        loggingService.Dispose();
 
         bool success = (UserFile.Exists && DeveloperFile.Exists);
+        Assert.True(success);
+        UserFile.Delete();
+        DeveloperFile.Delete();
+    }
 
-        Assert.True(success)
+    [Fact]
+    public void WriteLogEntryShouldWriteLog() {
+        FileLoggingService loggingService = new FileLoggingService("");
+        loggingService.WriteLogEntry("this is a user log", "this is a developer log");
 
+        FileInfo UserFile = new FileInfo("userLog.txt");
+        FileInfo DeveloperFile = new FileInfo("developerLog.txt");
+        loggingService.Dispose();
+
+        bool success = (File.ReadAllText(UserFile.FullName).Contains("this is a user log") && File.ReadAllText(DeveloperFile.FullName).Contains("this is a developer log"));
+        Assert.True(success);
+        UserFile.Delete();
+        DeveloperFile.Delete();
     }
 
 }
